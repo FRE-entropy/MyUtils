453736bc33521d49072873e75c645d570b510d3e
Entropy <Hzc299792458@outlook.com>
2024-01-28 19:33:03 +0800

    ArrayUtils

--- Test/main.cpp
+++ Test/main.cpp
@@ -10,11 +10,5 @@ int main()
 	std::cout << ArrayUtils::length(a) << std::endl;
 	std::cout << ArrayUtils::toString(a, "[", ", ", "]") << std::endl;
 	std::cout << ArrayUtils::toString(a) << std::endl;
-
-	std::set<int> b;
-
-	b.insert(std::begin(a), std::end(a));
-
-	std::cout << ArrayUtils::toString(b) << std::endl;
 	return 0;
 }
